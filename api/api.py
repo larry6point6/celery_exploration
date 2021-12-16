04072fe4544896c574999cdc151f403930ba2930
larry6point6 <larry.adeyemi@6point6.co.uk>
2021-12-16 17:29:10 +0000

    working on the skeleton of the API

--- api/api.py
+++ api/api.py
@@ -1,12 +1,16 @@
-import flask
 from flask import Flask, jsonify, request
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/", methods=["Get"])
 def home():
     return "Testing the early stages of the api, this works, changes made"
+
+
+@app.route("/long_process", methods=["Post"])
+def long_process():
+    ...
 
 
 if __name__ == "__main__":
